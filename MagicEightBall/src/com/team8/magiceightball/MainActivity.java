ec7e2ba89060f0bcd04e2c952e9eeb446666f250
thugnificent <josephthientran@gmail.com>
2013-06-23 18:13:04 -0400

    Fixed shaking feature. Had to modify position of text for some reason.

--- MagicEightBall/src/com/team8/magiceightball/MainActivity.java
+++ MagicEightBall/src/com/team8/magiceightball/MainActivity.java
@@ -6,38 +6,40 @@ import android.hardware.Sensor;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.view.Menu;
-import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 /**
  * Just a tester file to show what ResponseGenerator does.
  */
-public class MainActivity extends Activity implements View.OnClickListener {
+public class MainActivity extends Activity {
 	TextView responseField;
 	TextView instructions;
-	ImageView generateImage;   
+	ImageView generateImage;
 	private ShakeDetector mShakeDetector;
 	private SensorManager mSensorManager;
 	private Sensor mAccelerometer;
-	private boolean shaken = false;
-
-	
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
+
 		mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        
+		mAccelerometer = mSensorManager
+				.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
 
 		responseField = (TextView) findViewById(R.id.textView1);
 		instructions = (TextView) findViewById(R.id.textView2);
 		generateImage = (ImageView) findViewById(R.id.imageView1);
-		generateImage.setOnClickListener(this);
+
+		mShakeDetector = new ShakeDetector(new OnShakeListener() {
+
+			@Override
+			public void onShake() {
+				runShake();
+			}
+		});
 	}
 
 	@Override
@@ -46,40 +48,27 @@ public class MainActivity extends Activity implements View.OnClickListener {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-	
-	
-    @Override
-    protected void onResume() {
-        super.onResume();
-        mSensorManager.registerListener(mShakeDetector, mAccelerometer, SensorManager.SENSOR_DELAY_UI);
-    }
-    
-    @Override
-    protected void onPause() {
-        mSensorManager.unregisterListener(mShakeDetector);
-        super.onPause();
-    } 
 
 	@Override
-	public void onClick(View v) {
-		Toast.makeText(getBaseContext(), "Shake to receive fortune.", Toast.LENGTH_LONG).show();
-		
-		if (v.getId() == R.id.imageView1) {
-	        mShakeDetector = new ShakeDetector(new OnShakeListener() {
-	            @Override
-	            public void onShake() {
-	            	shaken = true;
-	            }
-	        });
-	        if (shaken == true) {
-	        	ResponseGenerator response = new ResponseGenerator();
-	    		responseField.setText("test");
-	        	
-				responseField.setText(response.getResponse());
-				Animate.startAnimation(this, R.id.imageView1);
-				
-				FadeIn.runAlphaAnimation(this, R.id.textView1);
-	        }
-		}
+	protected void onResume() {
+		super.onResume();
+		mSensorManager.registerListener(mShakeDetector, mAccelerometer,
+				SensorManager.SENSOR_DELAY_UI);
+	}
+
+	@Override
+	protected void onPause() {
+		mSensorManager.unregisterListener(mShakeDetector);
+		super.onPause();
+	}
+
+	public void runShake() {
+		ResponseGenerator response = new ResponseGenerator();
+		responseField.setText("test");
+
+		responseField.setText(response.getResponse());
+		Animate.startAnimation(this, R.id.imageView1);
+
+		FadeIn.runAlphaAnimation(this, R.id.textView1);
 	}
 }
